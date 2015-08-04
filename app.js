2c69b99d62605ccbefd37fb8306ad167ba2df5f9
Alan Fu <alanzfu@gmail.com>
2015-08-03 20:46:05 -0700

    Clean up readme and add comments

---
+++ app.js
@@ -0,0 +1,9 @@
+var Product = function(name, category, picture_url, price, selling_points){
+  this.name = name;
+  this.category = category;
+  this.picture_url = picture_url;
+  this.price = price;
+  this.selling_points = selling_points;
+}
+
+var product1 = new Book("Harry Potter", Book,  )
